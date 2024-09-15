11a5a9bc4ea8e88cd216f4e25fd68d6fd4914516
Cory <burnscory2001@gmail.com>
2024-09-15 13:32:42 -0400

    Added an environment variable for the API key

--- HelpBot/Games/RandomFact/Fact.cs
+++ HelpBot/Games/RandomFact/Fact.cs
@@ -6,10 +6,13 @@ namespace HelpBot.Games.RandomFact
 {
     public class Fact
     {
-        private readonly string APIKey = "GYXYu1A+F7SrIQKrek88ig==w8zYNp6XV6VmU3Qj";
+        private readonly string APIKey = Environment.GetEnvironmentVariable("API_KEY");
 
         public async Task<string> GetFact()
         {
+            if(string.IsNullOrEmpty(APIKey))
+                Console.WriteLine("API KEY IS NOT SET!");
+
             var client = new RestClient("https://api.api-ninjas.com/v1/facts");
             var request = new RestRequest()
             {
